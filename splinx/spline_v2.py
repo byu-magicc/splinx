49eefc9ac878649aee29fc774c7859d2128ee277
James Usevitch <james_usevitch@byu.edu>
2025-02-01 22:19:51 -0700

    Added docstrings

--- splinx/spline_v2.py
+++ splinx/spline_v2.py
@@ -15,6 +15,54 @@ from timeit import timeit
 # @jit
 @partial(jit, static_argnums=(3,))
 def basis(t, knots, i, k):
+    """
+    This function evaluates a single B-spline basis function N_{i,k} at t.
+
+    To evaluate an array of basis functions and / or a batch of times,
+    use jax.vmap to vectorize this function.
+
+    For example, to map over all basis functions and evaluate them at a single time t:
+
+        ```
+        n = 40
+        k = 5
+        t = 1.5
+
+        # Define the knot vector
+        knots = jnp.arange(n+k+1)
+
+        indices = jnp.arange(n)
+        basis_funcs = jax.vmap(lambda i: basis(t, knots, i, k), in_axes=0)(indices)
+        ```
+
+    To map over all basis functions _and_ evaluate them at an array of times t (i.e. a batch):
+
+        ```
+        # (...add the code above...)
+
+        t = jnp.linspace(knots[0], knots[-1], 1000).reshape(-1, 1)
+        basis_funcs = jax.vmap(jax.vmap(lambda i: basis(t, knots, i, k), in_axes=0), in_axes=0)(indices)
+        ```
+        
+    Args:
+    -----
+    t: float
+        The time at which to evaluate the basis function
+    knots: jnp.ndarray
+        The knot vector
+    i: int
+        The index of the basis function
+    k: int
+        The degree of the basis function
+
+    Returns:
+    --------
+
+    The value of the basis function at t.
+
+
+
+    """
     if k == 0:
         condition = (knots[i] <= t) & (t < knots[i+1])
         return jnp.where(condition, 1.0, 0.0)
